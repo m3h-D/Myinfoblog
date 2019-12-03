5247a5dcfae3fd18115d4e80c65057ff7e3aeda6
Mahdi <huskar_assassin@yahoo.com>
2019-12-04 02:55:13 +0330

    Added Like counter

--- src/likes/templatetags/like_tags.py
+++ src/likes/templatetags/like_tags.py
@@ -45,3 +45,19 @@ def dislike_btn_color(context, id, model_type):
                 return True
     except:
         return False
+
+
+@register.simple_tag
+def like_counter(id, model_type):
+    content_type = ContentType.objects.get(model=model_type)
+    like = LikeDislike.objects.filter(
+        content_type=content_type, object_id=id).filter(likedislike='like')
+    return like.count()
+
+
+@register.simple_tag
+def dislike_counter(id, model_type):
+    content_type = ContentType.objects.get(model=model_type)
+    like = LikeDislike.objects.filter(
+        content_type=content_type, object_id=id).filter(likedislike='dislike')
+    return like.count()
